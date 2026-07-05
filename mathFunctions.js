c42b36deb8bb0cc0364f2dda6aeab9ff4ee7a50d
Adyagit24 <adiparija24@gmail.com>
2026-07-05 19:45:41 +0530

    Add mathFunctions.js with add and subtract functions

---
+++ mathFunctions.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  return a + b;
+}
+
+// subtract two numbers
+function subtract(a, b) {
+  return a - b;
+}
+
+//Export the functions
+module.exports = {
+  add,
+  subtract
+};
