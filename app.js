0acc7eaeef22b588c64cc8c57cffbd8a81a845d3
Adyagit24 <adiparija24@gmail.com>
2026-07-05 19:47:06 +0530

    Add app.js to demonstrate math functions

---
+++ app.js
@@ -0,0 +1,7 @@
+ const { add, subtract} = require('./mathFunctions');
+
+const sum = add(5, 3);
+const difference = subtract(10, 4);
+
+console.log(`Sum: ${sum}`);
+console.log(`Difference: ${difference}`);
